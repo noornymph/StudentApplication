cb0bc1c82fe7359ee4f8770787ed45db6d4745a7
TaniaAllahBakhsh <khadijaaranoor2002@gmail.com>
2023-07-27 08:13:07 +0500

    change

--- app/src/main/java/com/example/assignment02/modelTask.java
+++ app/src/main/java/com/example/assignment02/modelTask.java
@@ -4,42 +4,42 @@ public class modelTask {
 
     private String sabaq,sabaqi,manzil,rollNum;
 
-    public modelTask(String sabaq, String sabaqi, String manzil, String rollNum) {
+     modelTask(String sabaq, String sabaqi, String manzil, String rollNum) {
         this.sabaq = sabaq;
         this.sabaqi = sabaqi;
         this.manzil = manzil;
         this.rollNum = rollNum;
     }
 
-    public String getSabaq() {
+     String getSabaq() {
         return sabaq;
     }
 
-    public void setSabaq(String sabaq) {
+     void setSabaq(String sabaq) {
         this.sabaq = sabaq;
     }
 
-    public String getSabaqi() {
+     String getSabaqi() {
         return sabaqi;
     }
 
-    public void setSabaqi(String sabaqi) {
+     setSabaqi(String sabaqi) {
         this.sabaqi = sabaqi;
     }
 
-    public String getManzil() {
+     String getManzil() {
         return manzil;
     }
 
-    public void setManzil(String manzil) {
+     void setManzil(String manzil) {
         this.manzil = manzil;
     }
 
-    public String getRollNum() {
+     String getRollNum() {
         return rollNum;
     }
 
-    public void setRollNum(String rollNum) {
+     void setRollNum(String rollNum) {
         this.rollNum = rollNum;
     }
 }
